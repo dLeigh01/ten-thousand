88d638b21b42999f98b1554b22ada18cae98b752
Dana Huffman <dana.huffman@yahoo.com>
2022-07-25 13:23:53 -0700

    Passed dice roll tests

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -1,2 +1,19 @@
-class Gamelogic:
-    pass
+from collections import Counter
+import random
+
+class GameLogic:
+    @staticmethod
+    def calculate_score(roll):
+        score = 0
+        sort = Counter(roll).most_common
+
+        return score
+
+    @staticmethod
+    def roll_dice(int):
+        result = []
+        i = 0
+        while i < int:
+            result.append(1)
+            i += 1
+        return tuple(result)
